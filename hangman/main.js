66e174d8b23d61e3bb70c6c031f08d218294c74c
Soulburger <66534972+Soulburger@users.noreply.github.com>
2021-08-05 03:45:19 -0400

    Got rid of some SCSS stuff that doesn't exist because I'm not using SCSS

--- hangman/main.js
+++ hangman/main.js
@@ -107,13 +107,7 @@ function randomWord() {
 function generateButtons() {
   let buttonsHTML = 'abcdefghijklmnopqrstuvwxyz'.split('').map(letter =>
     `
-      <button
-        class="btn btn-lg btn-primary m-2"
-        id='` + letter + `'
-        onClick="handleGuess('` + letter + `')"
-      >
-        ` + letter + `
-      </button>
+      <button id='` + letter + `' onClick="handleGuess('` + letter + `')"> ` + letter + ` </button>
     `).join('');
 
   document.getElementById('keyboard').innerHTML = buttonsHTML;
